0333f534a06f8f4542184b7e3f662c217bfff55d
vgrau98 <victor.grau93@gmail.com>
2021-12-26 23:13:37 +0100

    feat : retrieve tmperature ms ressources

--- src/main/java/com/insa/projet/microservices/orchestrator_MS/controller/OrchestratorRessource.java
+++ src/main/java/com/insa/projet/microservices/orchestrator_MS/controller/OrchestratorRessource.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.insa.projet.microservices.orchestrator_MS.model.temperature.TemperatureSensor;
 
+@RestController
 public class OrchestratorRessource {
 	
 	@PostMapping("/init/{n}")
