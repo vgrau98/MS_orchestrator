1ca299ca031b21a2bd11058dd308fff822a88c27
vgrau98 <victor.grau93@gmail.com>
2021-12-26 23:23:53 +0100

    feat : retrieve tmperature ms ressources

--- src/main/java/com/insa/projet/microservices/orchestrator_MS/controller/OrchestratorRessource.java
+++ src/main/java/com/insa/projet/microservices/orchestrator_MS/controller/OrchestratorRessource.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.insa.projet.microservices.orchestrator_MS.model.nbrPeople.NbrPeopleSensor;
 import com.insa.projet.microservices.orchestrator_MS.model.temperature.TemperatureSensor;
+import com.insa.projet.microservices.orchestrator_MS.model.window.Window;
 
 @RestController
 public class OrchestratorRessource {
@@ -19,13 +21,31 @@ public class OrchestratorRessource {
 		RestTemplate restTemplate=new RestTemplate();
 		
 		restTemplate.postForObject("http://localhost:8080/temperature/init/"+String.valueOf(n), null, List.class);
+		restTemplate.postForObject("http://localhost:8080/window/init/"+String.valueOf(n), null, List.class);
+		restTemplate.postForObject("http://localhost:8080/NbrPeople/init/"+String.valueOf(n), null, List.class);
+
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("/listTemperature")
 	public List<TemperatureSensor> getListTemperatureSensor(){
 		RestTemplate restTemplate=new RestTemplate();
 		List<TemperatureSensor> listSensor;
 		listSensor=restTemplate.getForObject("http://localhost:8080/temperature/list", List.class);
+		return listSensor;
+	}
+	
+	@GetMapping("/listWindow")
+	public List<Window> getListWindow(){
+		RestTemplate restTemplate=new RestTemplate();
+		List<Window> listWindow;
+		listWindow=restTemplate.getForObject("http://localhost:8080/window/list", List.class);
+		return listWindow;
+	}
+	@GetMapping("/listNbrPeopleSensor")
+	public List<NbrPeopleSensor> getListNbrPeopleSensor(){
+		RestTemplate restTemplate=new RestTemplate();
+		List<NbrPeopleSensor> listSensor;
+		listSensor=restTemplate.getForObject("http://localhost:8080/nbrPeopleSensor/list", List.class);
 		return listSensor;
 	}
 
