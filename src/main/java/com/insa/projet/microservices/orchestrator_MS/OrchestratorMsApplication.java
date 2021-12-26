02909c76a1efbb3cc171e2ad38cafc814ca60131
vgrau98 <victor.grau93@gmail.com>
2021-12-26 22:32:17 +0100

    first commit

---
+++ src/main/java/com/insa/projet/microservices/orchestrator_MS/OrchestratorMsApplication.java
@@ -0,0 +1,13 @@
+package com.insa.projet.microservices.orchestrator_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestratorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insa/projet/microservices/orchestrator_MS/OrchestratorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insa.projet.microservices.orchestrator_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
