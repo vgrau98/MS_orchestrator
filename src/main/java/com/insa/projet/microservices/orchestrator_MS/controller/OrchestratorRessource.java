cdc2b53be96167a6278de8869ebfb483d1977e44
vgrau98 <victor.grau93@gmail.com>
2021-12-29 17:26:26 +0100

    feat: discovery with eureka under jenkins

--- src/main/java/com/insa/projet/microservices/orchestrator_MS/controller/OrchestratorRessource.java
+++ src/main/java/com/insa/projet/microservices/orchestrator_MS/controller/OrchestratorRessource.java
@@ -2,6 +2,8 @@ package com.insa.projet.microservices.orchestrator_MS.controller;
 
 import java.util.List;
 
+import javax.print.attribute.standard.Media;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
@@ -28,51 +30,42 @@ public class OrchestratorRessource {
 	public void initDataBase(@PathVariable int n) {
 		
 		
-		//restTemplate.postForObject("http://localhost:8080/temperature/init/"+String.valueOf(n), null, List.class);
-		//restTemplate.postForObject("http://localhost:8080/window/init/"+String.valueOf(n), null, List.class);
-		//restTemplate.postForObject("http://localhost:8080/nbrPeopleSensor/init/"+String.valueOf(n), null, List.class);
-
 		restTemplate.postForObject("http://TemperatureSensorsService/temperature/init/"+String.valueOf(n), null, List.class);
-		//restTemplate.postForObject("http://localhost:8080/window/init/"+String.valueOf(n), null, List.class);
-		//restTemplate.postForObject("http://localhost:8080/nbrPeopleSensor/init/"+String.valueOf(n), null, List.class);
+		restTemplate.postForObject("http://WindowManagementService/window/init/"+String.valueOf(n), null, List.class);
+		restTemplate.postForObject("http://NbrPeopleSensorsService/nbrPeopleSensor/init/"+String.valueOf(n), null, List.class);
 
 	}
 	
 	@GetMapping(path="/listTemperature", produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<TemperatureSensor> getListTemperatureSensor(){
 		List<TemperatureSensor> listSensor;
-		//listSensor=restTemplate.getForObject("http://localhost:8080/temperature/list", List.class);
 		listSensor=restTemplate.getForObject("http://TemperatureSensorsService/temperature/list", List.class);
 		return listSensor;
 	}
 	
-	@GetMapping("/listWindow")
+	@GetMapping(path="/listWindow", produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Window> getListWindow(){
-		RestTemplate restTemplate=new RestTemplate();
 		List<Window> listWindow;
-		listWindow=restTemplate.getForObject("http://localhost:8080/window/list", List.class);
+		listWindow=restTemplate.getForObject("http://WindowManagementService/window/list", List.class);
 		return listWindow;
 	}
-	@GetMapping("/listNbrPeopleSensor")
+	@GetMapping(path="/listNbrPeopleSensor", produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<NbrPeopleSensor> getListNbrPeopleSensor(){
-		RestTemplate restTemplate=new RestTemplate();
 		List<NbrPeopleSensor> listSensor;
-		listSensor=restTemplate.getForObject("http://localhost:8080/nbrPeopleSensor/list", List.class);
+		listSensor=restTemplate.getForObject("http://NbrPeopleSensorsService/nbrPeopleSensor/list", List.class);
 		return listSensor;
 	}
 	
 	@PostMapping(path="/addTemperatureValueRoom/{room}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void addTemperatureValue(@RequestBody SensorValue value,@PathVariable int room) {
-		RestTemplate restTemplate = new RestTemplate();
 		HttpEntity<SensorValue> httpEntity= new HttpEntity<SensorValue>(value, null);
-		restTemplate.postForObject("http://localhost:8080/temperature/addValueRoom/"+String.valueOf(room), value, Object.class);
+		restTemplate.postForObject("http://TemperatureSensorsService/temperature/addValueRoom/"+String.valueOf(room), value, Object.class);
 		
 	}
 	
 	@PostMapping(path="/ManageWindow/{outdoor_threshold}/{delta_temp_threshold}/{threshold_nbrPeople}")
 	public void windowManager(@PathVariable double outdoor_threshold,@PathVariable double delta_temp_threshold, @PathVariable int threshold_nbrPeople) {
 		
-		RestTemplate restTemplate=new RestTemplate();
 		List<Window> listWindow;
 		List<TemperatureSensor> listTempSensor;
 		List<NbrPeopleSensor> listNbrPeopleSensor;
@@ -85,10 +78,10 @@ public class OrchestratorRessource {
 			
 			boolean windowState=false;
 			
-			double outdoorTemp=restTemplate.getForObject("http://localhost:8080/temperature/room/-1", TemperatureSensor.class).getValues().get(-1).getValue();
+			double outdoorTemp=restTemplate.getForObject("http://TemperatureSensorsService/temperature/room/-1", TemperatureSensor.class).getValues().get(-1).getValue();
 			int room=listWindow.get(i).getRoom();
-			double indoorTemp=restTemplate.getForObject("http://localhost:8080/temperature/room/"+String.valueOf(room), TemperatureSensor.class).getValues().get(-1).getValue();
-			int nbrPeople=restTemplate.getForObject("http://localhost:8080/nbrPeopleSensor/room/"+String.valueOf(room), NbrPeopleSensor.class).getValues().get(-1).getNbrPeople();
+			double indoorTemp=restTemplate.getForObject("http://TemperatureSensorsService/temperature/room/"+String.valueOf(room), TemperatureSensor.class).getValues().get(-1).getValue();
+			int nbrPeople=restTemplate.getForObject("http://NbrPeopleSensorsService/nbrPeopleSensor/room/"+String.valueOf(room), NbrPeopleSensor.class).getValues().get(-1).getNbrPeople();
 			
 			double delta_temp=Math.abs(outdoorTemp-indoorTemp);
 			
@@ -99,7 +92,7 @@ public class OrchestratorRessource {
 			
 			WindowState ws = new WindowState(windowState, System.currentTimeMillis());
 			HttpEntity<WindowState> httpEntity = new HttpEntity<>(ws, null);
-			restTemplate.postForObject("http://localhost:8080/window/addStateRoom/"+String.valueOf(room),httpEntity, Object.class);
+			restTemplate.postForObject("http://WindowManagementService/window/addStateRoom/"+String.valueOf(room),httpEntity, Object.class);
 			
 		}
 		
